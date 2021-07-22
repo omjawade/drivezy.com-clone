7923fa4a5e9291d93872bbeaf47f8facbc74a7f1
Nil patil <nilp7744@gmail.com>
2021-07-22 12:47:30 +0530

    added payment cart

--- src/Pages/PaymentConfirmation/CarPriceD.jsx
+++ src/Pages/PaymentConfirmation/CarPriceD.jsx
@@ -7,31 +7,32 @@ function CarPriceD(props) {
             <div className="CarPrice-maindiv">
                 <div>Fare Details</div>
                 <br/>
-                <div className="fare-block">
-                    <span className="fare-name">Weekday Charges</span>
-                    <span className="fare-amout">₹466.40</span>
+                <div className="fare-block" style={{marginTop: "-10px"}} >
+                    <h4 className="fare-name">Weekday Charges</h4>
+                    <p className="fare-amout">₹466.40</p>
                 </div>
-              
-                 <div className="fare-block">
-                    <span className="fare-name">Security Deposit</span>
-                    <span className="fare-amout">₹0</span>
+               
+                 <div className="fare-block" style={{marginTop: "-30px"}}>
+                    <h4 className="fare-name">Security Deposit</h4>
+                    <p className="fare-amout">₹0</p>
                 </div>
-                <hr/>
+                
+                <div style={{ borderBottom:"1px solid gray",marginTop: "-10px"}}></div>
 
                 <div className="fare-block">
-                    <span className="fare-name">Total Fare</span>
-                    <span className="fare-amout">₹0</span>
+                    <h4 className="fare-name">Total Fare</h4>
+                    <h4 className="fare-amout">₹466.40</h4>
                 </div>
 
 
                 <div className="fare-block">
                    
-                    <li>This price is rounded off and is inclusive of GST</li>
+                    <li style={{ marginTop: "-10px",fontFamily:"font-family: 'Open Sans', sans-serif" ,color:"gray",fontSize:"12px",fontweight:"bold"}}>This price is rounded off and is inclusive of GST</li>
                   
                 </div>
                
                 <div className="fare-block">
-                    <span className="fare-name">Have a Coupon? LOGIN</span>
+                    <span className="fare-name"><a style={{color:"black"}}>Have a Coupon ?</a> LOGIN</span>
                   
                 </div>
             </div>
